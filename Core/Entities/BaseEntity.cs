d89a8e1697d09e9cfcd12dfaf9e83a72f6e86175
sahan <101819553+omerfshan@users.noreply.github.com>
2025-06-09 01:41:54 +0300

    Game class yazıldı ve coreda base Entity yazıldı

---
+++ Core/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+namespace Core.Entities;
+
+public class BaseEntity<TId>
+{
+    public TId? Id { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
+}
---
+++ Entities/Game.cs
@@ -0,0 +1,14 @@
+﻿
+using Core.Entities;
+
+namespace Entities;
+
+
+
+public class Game : BaseEntity<int>
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? ThumbnailUrl { get; set; }
+    public DateTime ReleaseDate { get; set; }
+}
